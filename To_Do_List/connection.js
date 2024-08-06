e61a7c37c99c8ec3e66ebff91461c27158c43255
kkiduss <kidustesfaye92@gmail.com>
2024-08-06 23:17:20 +0300

    first commit

---
+++ To_Do_List/connection.js
@@ -0,0 +1,9 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+    window.location.href = 'index.html'; // Replace 'dashboard.html' with your target page
+});
+document.getElementById('signup').addEventListener('click', () => {
+    window.location.href = 'login.html'; // Replace 'dashboard.html' with your target page
+});
+document.getElementById('loginButton').addEventListener('click', () => {
+    window.location.href = 'index.html'; // Replace 'dashboard.html' with your target page
+});
